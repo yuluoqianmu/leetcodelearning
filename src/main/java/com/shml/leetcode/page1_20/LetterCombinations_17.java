adecced09130fc1436e86ad8685ad8855861d0d7
yuluoqianmu <350083890@qq.com>
2019-12-23 08:21:35 +0800

    第17题 电话号码的字母组合

--- src/main/java/com/shml/leetcode/page1_20/LetterCombinations_17.java
+++ src/main/java/com/shml/leetcode/page1_20/LetterCombinations_17.java
@@ -1,7 +1,74 @@
 package com.shml.leetcode.page1_20;
 
-/**
- * Created by shaoml on 2019/9/11.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * ### [17\. 电话号码的字母组合](https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/)
+ Difficulty: **中等**
+
+ 给定一个仅包含数字 `2-9` 的字符串，返回所有它能表示的字母组合。
+
+ 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+ ![](https://assets.leetcode-cn.com/aliyun-lc-upload/original_images/17_telephone_keypad.png)
+
+ **示例:**
+ ```
+ 输入："23"
+ 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ ```
+ **说明:**
+ 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
  */
 public class LetterCombinations_17 {
+
+    public static Map<String, String> phone = new HashMap<String, String>();
+    static {
+       phone.put("2", "abc");
+       phone.put("3", "def");
+       phone.put("4", "ghi");
+       phone.put("5", "jkl");
+       phone.put("6", "mno");
+       phone.put("7", "pqrs");
+       phone.put("8", "tuv");
+       phone.put("9", "wxyz");
+    };
+
+   public static List<String> output = new ArrayList<>();
+
+    public static List<String> letterCombinations(String digits) {
+        if (digits.length() != 0){
+            backtrack("", digits);
+        }
+        return output;
+    }
+
+    /**
+     * 回溯方法
+     * @param combination
+     * @param next_digits
+     */
+    public static void backtrack(String combination, String next_digits) {
+        if (next_digits.length() == 0) {
+            output.add(combination);
+        } else {
+            String digit = next_digits.substring(0, 1);
+            String letters = phone.get(digit);
+            for (int i=0; i<letters.length(); i++){
+                String letter = phone.get(digit).substring(i, i+1);
+                backtrack(combination + letter, next_digits.substring(1));
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<String> ret =  letterCombinations("23");
+        for (String str : ret){
+            System.out.println(str);
+        }
+    }
 }
