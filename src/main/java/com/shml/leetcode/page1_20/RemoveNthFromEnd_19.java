f4bd3a3d6f6194acbe782299ae8d5d5bb0eef9da
yuluoqianmu <350083890@qq.com>
2019-12-30 00:26:23 +0800

    第19题 删除链表的倒数第N个节点
    Signed-off-by: yuluoqianmu <350083890@qq.com>

--- src/main/java/com/shml/leetcode/page1_20/RemoveNthFromEnd_19.java
+++ src/main/java/com/shml/leetcode/page1_20/RemoveNthFromEnd_19.java
@@ -1,10 +1,32 @@
 package com.shml.leetcode.page1_20;
 
+/**
+ * ### [19\. 删除链表的倒数第N个节点](https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/)
+
+ Difficulty: **中等**
+
+
+ 给定一个链表，删除链表的倒数第 _n _个节点，并且返回链表的头结点。
+
+ **示例：**
+
+ ```
+ 给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+ 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ ```
+ */
 
 import com.shml.leetcode.common.ListNode;
 
 public class RemoveNthFromEnd_19 {
 
+    /**
+     * 思路：使用以前以后两个指针，两个指针的距离相差n
+     * @param head
+     * @param n
+     * @return
+     */
     public static ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
