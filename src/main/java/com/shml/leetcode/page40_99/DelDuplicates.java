1e1c62a5a79227f14f069eaa8da9463c953d792d
shaomaolin <350083890@qq.com>
2021-03-26 13:47:21 +0800

    83. 删除排序链表中的重复元素

---
+++ src/main/java/com/shml/leetcode/page40_99/DelDuplicates.java
@@ -0,0 +1,28 @@
+package com.shml.leetcode.page40_99;
+
+import com.shml.leetcode.common.ListNode;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 返回同样按升序排列的结果链表。
+ */
+public class DelDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+}
